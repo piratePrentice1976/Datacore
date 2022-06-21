3c0eceaf1e4b48b955ee2567bb9e36622a254d60
James Jackson <piratePrentice1976@gmail.com>
2022-06-21 09:36:57 -0400

    Node packages

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'C:\Users\jjackson\Documents\Projects\Datacore',
+	globPatterns: [
+		'**/*.{html,txt,css,eot,svg,ttf,woff,woff2,otf,js,jpg,png,json,md,scss}'
+	],
+	swDest: 'C:/Users/jjackson/Documents/Projects/Datacore/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
